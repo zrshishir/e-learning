b1c991daa5a200b8b0979c13d867eaa0655f098f
zrshishir <zrshishir@gmail.com>
2020-06-18 00:27:55 +0600

    lesson orm relationship

--- app/Model/BasicTable/Lesson.php
+++ app/Model/BasicTable/Lesson.php
@@ -8,4 +8,16 @@ class Lesson extends Model
 {
     protected $table = "lessons";
     protected $fillable = ['user_id', 'course_id', 'name', 'active'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function course(){
+        return $this->belongsTo('App\Model\BasicTable\Course');
+    }
+
+    public function questions(){
+        return $this->hasMany('App\Model\BasicTable\Questions');
+    }
 }
