29f3bdfc3e3f111051688b7d3c45849ec523d662
Ziaur Rahman <zrshishir@gmail.com>
2020-10-30 08:48:27 +0600

    resolving middleware issue

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\PostmanCheck::class,
+        // \App\Http\Middleware\PostmanCheck::class,
     ];
 
     /**
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'admin' => \App\Http\Middleware\Admincheck::class,
+        'admin' => \App\Http\Middleware\AdminCheck::class,
     ];
 }
